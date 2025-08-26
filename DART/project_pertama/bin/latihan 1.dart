a8f3ddf49b473df3e3643220981731aecf38ebb6
Taufiq djabir <mdjabirtaufiq@gmail.com>
2025-08-26 13:21:14 +0900

    biodata

---
+++ DART/project_pertama/bin/latihan 1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Taufiq M";
+  int age = 20;
+  double height = 164 ;
+  num weight = 53.40;
+  dynamic kampus = "jati";
+  
+  print ("Nama : $name");
+  print ("Umur : $age");
+  print ("TinggiBadan : $height");
+  print ("BeratBadan : $weight");
+  print ("kampus : $kampus");
+}
